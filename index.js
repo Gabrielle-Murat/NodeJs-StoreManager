39ba488f5850224576447cfcfa4a00b04e678451
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-29 16:10:48 -0300

    Adiciona rota de products ao index

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const bodyParser = require('body-parser');
 const app = require('./app');
 require('dotenv').config();
+
+app.use(bodyParser.json());
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
+
+const productsRouter = require('./middlewares/productsRouter');
+
+app.use('/products', productsRouter);

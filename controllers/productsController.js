1aa9fd71c65030238601455fd769c6a74e6e4441
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-29 15:53:34 -0300

    Cria funcao para buscar um produto por Id - Camada Controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -10,6 +10,16 @@ const getProducts = async (_req, res) => {
   return res.status(200).json(productsList);
 };
 
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const product = await Products.getProductById(id);
+
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+
+  return res.status(200).json(product);
+};
+
 module.exports = {
   getProducts,
+  getProductById,
 };

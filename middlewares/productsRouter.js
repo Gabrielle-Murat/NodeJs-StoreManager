c95b0c70a58eb1a5050c3dccf29dcda4ddb716b9
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-29 16:09:54 -0300

    Adiciona rotas para listar todos os produtos e pegar produto por Id

--- middlewares/productsRouter.js
+++ middlewares/productsRouter.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const Products = require('../controllers/productsController');
+
 const router = express.Router();
+
+router.get('/', Products.getProducts);
+
+router.get('/:id', Products.getProductById);
 
 module.exports = router;

573bfb00f9092aa53044afe2997dd4e0e5d35b91
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-03 21:50:22 -0300

    Cria funcao para listar todas as vendas do db - Camada Controller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,3 +1,15 @@
 const Sales = require('../services/salesService');
 
-module.exports = {};
+// requisito 08
+
+const getSales = async (req, res) => {
+  const salesList = await Sales.getSales();
+
+  if (!salesList) return res.status(404).json({ message: 'No sales found' });
+
+  return res.status(200).json(salesList);
+};
+
+module.exports = {
+  getSales,
+};

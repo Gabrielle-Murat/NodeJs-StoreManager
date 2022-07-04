f2677c835e6a48c5def55d168201f8d28bf2169b
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-03 21:49:55 -0300

    Cria funcao para listar todas as vendas do db - Camada Model

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,3 +1,33 @@
 const connection = require('./connection');
 
-module.exports = {};
+// conversão de snake_case para camel_case, course dia 23.1
+const serialize = (saleData) => ({
+  saleId: saleData.sale_id,
+  productId: saleData.product_id,
+  quantity: saleData.quantity,
+  date: saleData.date,
+});
+
+// requisito 08
+
+const getSales = async () => {
+  const getSalesQuery = `
+    SELECT
+      sp.sale_id,
+      s.date,
+      sp.product_id,
+      sp.quantity
+    FROM
+      StoreManager.sales AS s
+        INNER JOIN
+      StoreManager.sales_products AS sp ON s.id = sp.sale_id;
+  `;
+
+  const [salesList] = await connection.execute(getSalesQuery);
+  
+  return salesList.map(serialize);
+};
+
+module.exports = {
+  getSales,
+};

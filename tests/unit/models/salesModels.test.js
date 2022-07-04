72bee04d4487557c160572c0096b0fe77032578a
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-04 17:44:31 -0300

    Adiciona testes para busca de vendas por Id - Camada Model

--- tests/unit/models/salesModels.test.js
+++ tests/unit/models/salesModels.test.js
@@ -38,5 +38,54 @@ describe('Model de Sales:', () => {
     });
   });
 
-  // describe('2 - Busca vendas por Id:', () => {});
+  describe('2 - Busca vendas por Id:', () => {
+    describe('2.1 - Caso de sucesso', () => {
+      const idMock = 1;
+      const model = [
+        [
+          {
+            "date": "2021-09-09T04:54:29.000Z",
+            "productId": 1,
+            "quantity": 2
+          },
+          {
+            "date": "2021-09-09T04:54:54.000Z",
+            "productId": 2,
+            "quantity": 2
+          },
+        ],
+      ];
+
+      beforeEach(() => {
+        sinon.stub(connection, 'execute').resolves(model);
+      });
+
+      afterEach(() => {
+        connection.execute.restore();
+      });
+
+      it('Será validado que é retornado um array de objetos que representa uma venda', async () => {
+        const received = await salesModel.getSalesById(idMock);
+        expect(received).to.be.an('array');
+      });
+    });
+
+    describe('2.2 - Caso de falha', () => {
+      const idMock = 0;
+      const model = [[]];
+
+      beforeEach(() => {
+        sinon.stub(connection, 'execute').resolves(model);
+      });
+
+      afterEach(() => {
+        connection.execute.restore();
+      });
+
+      it('Será validado que é retornado "null"', async () => {
+        const received = await salesModel.getSalesById(idMock);
+        expect(received).to.be.null;
+      });
+    });
+  });
 });

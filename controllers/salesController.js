5c5f91dfa8802a8c239575eef9d59ec3a97c409e
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-03 22:06:21 -0300

    Cria funcao para buscar vendas por Id - Camada Controller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -2,7 +2,7 @@ const Sales = require('../services/salesService');
 
 // requisito 08
 
-const getSales = async (req, res) => {
+const getSales = async (_req, res) => {
   const salesList = await Sales.getSales();
 
   if (!salesList) return res.status(404).json({ message: 'No sales found' });
@@ -10,6 +10,16 @@ const getSales = async (req, res) => {
   return res.status(200).json(salesList);
 };
 
+const getSalesById = async (req, res) => {
+  const { id } = req.params;
+  const sales = await Sales.getSalesById(id);
+
+  if (!sales) return res.status(404).json({ message: 'Sale not found' });
+
+  return res.status(200).json(sales);
+};
+
 module.exports = {
   getSales,
+  getSalesById,
 };

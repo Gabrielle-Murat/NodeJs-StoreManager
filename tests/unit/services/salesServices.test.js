788f895c946cc7e10b0ba4dca445458cd4c0c36e
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-04 17:44:42 -0300

    Adiciona testes para busca de vendas por Id - Camada Service

--- tests/unit/services/salesServices.test.js
+++ tests/unit/services/salesServices.test.js
@@ -41,5 +41,47 @@ describe('Service de Sales:', () => {
     });
   });
   
-  // describe('2 - Busca vendas por Id:', () => {});
+  describe('2 - Busca vendas por Id:', () => {
+    const idMock = 1;
+    const model = [
+      {
+        "date": "2021-09-09T04:54:29.000Z",
+        "productId": 1,
+        "quantity": 2
+      },
+      {
+        "date": "2021-09-09T04:54:54.000Z",
+        "productId": 2,
+        "quantity": 2
+      },
+    ];
+
+    beforeEach(() => {
+      sinon.stub(salesModel, 'getSalesById').resolves(model);
+    });
+
+    afterEach(() => {
+      salesModel.getSalesById.restore();
+    });
+
+    it('Será validado que é retornado um array de objetos que representa uma venda', async () => {
+      const received = await salesService.getSalesById(idMock);
+      expect(received).to.be.an('array');
+    });
+
+    it('Será validado que o array possui objetos que representam a venda individualmente', async () => {
+      const [received] = await salesService.getSalesById(idMock);
+      expect(received).to.be.an('object');
+    });
+
+    it('Será validado que o array possui todos os objetos que representam uma venda', async () => {
+      const received = await salesService.getSalesById(idMock);
+      expect(received).to.have.length(2);
+    });
+
+    it('Será validado que os objetos retornados possuem as chaves "date", "productId" e "quantity"', async () => {
+      const [received] = await salesService.getSalesById(idMock);
+      expect(received).to.include.all.keys('date', 'productId', 'quantity');
+    });
+  });
 });

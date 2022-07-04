5ed81c090014c676132d45427a344f601e2ae9eb
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-03 21:50:11 -0300

    Cria funcao para listar todas as vendas do db - Camada Service

--- services/salesService.js
+++ services/salesService.js
@@ -1,3 +1,13 @@
 const Sales = require('../models/salesModel');
 
-module.exports = {};
+// requisito 08
+
+const getSales = async () => {
+  const salesList = await Sales.getSales();
+
+  return salesList;
+};
+
+module.exports = {
+  getSales,
+};

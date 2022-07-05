080be6fbfcaf8bcf3de4bfbd3d1f20db7c3386f7
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-04 21:34:21 -0300

    Cria funcao para cadastrar novas vendas no db - Camada Service

--- services/salesService.js
+++ services/salesService.js
@@ -1,4 +1,5 @@
 const Sales = require('../models/salesModel');
+const Products = require('../models/productsModel');
 
 // requisito 08
 
@@ -14,7 +15,39 @@ const getSalesById = async (id) => {
   return sales;
 };
 
+// requisito 06
+
+const productIdExistanceCheck = async (saleArray) => {
+  const allProducts = await Products.getProducts();
+
+  const allDbProductsIds = allProducts.map((sale) => sale.id);
+  const productsIdsFromRequest = saleArray.map((sale) => sale.productId);
+  
+  const productIdIsNull = productsIdsFromRequest
+    .some((id) => !allDbProductsIds.includes(id));
+  
+  if (productIdIsNull) return null;
+};
+
+const createSale = async (saleArray) => {
+  const check = await productIdExistanceCheck(saleArray);
+  if (check === null) return 'not found';
+
+  const sale = await Sales.createSaleOnTableSales();
+  const response = {
+    id: sale.insertId,
+    itemsSold: saleArray,
+  };
+
+  saleArray.forEach(async ({ productId, quantity }) => {
+    await Sales.createSaleOnTableSalesProducts(sale.insertId, productId, quantity);
+  });
+
+  return response;
+};
+
 module.exports = {
   getSales,
   getSalesById,
+  createSale,
 };
